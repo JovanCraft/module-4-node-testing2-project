59830fae16b73f81a89d66fdc3f143e899dbf92b
jovan <jovancraft@gmail.com>
2024-02-22 07:57:03 -0600

    got my getAllProducts to pass the test

--- api/products/products-model.spec.js
+++ api/products/products-model.spec.js
@@ -5,11 +5,31 @@ const server = require('../server')
 const testProduct1 = { name: 'Markers', type: "Stationary", color: "black", quantity: 6, price: 3.99, brand: "Sharpie", size: "5.9 inch" }
 const testProduct2 = { name: 'Pencils', type: "Stationary", color: "yellow", quantity: 20, price: 2.94, brand: "Ticonderoga", size: "7.5 inch"}
 
-// beforeAll(async () => {
-//     await db.migrate.rollback()
-//     await db.migrate.latest
-// })
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
 
-it('checks for the correct env', () => {
+beforeEach(async () => {
+    await db('products').truncate()
+})
+
+afterAll(async () => {
+    await db.destroy()
+})
+
+test('checks for the correct env variable', () => {
     expect(process.env.DB_ENV).toBe('testing')
+})
+
+describe('Store Products', () => {
+    test('getAllProducts give us all the products currently in our store', async () => {
+        await request(server).post('/products').send(testProduct1);
+        await request(server).post('/products').send(testProduct2);
+
+        const response = await request(server).get('/products');
+
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(2);
+    })
 })
--- api/products/products-router.js
+++ api/products/products-router.js
@@ -2,10 +2,54 @@ const express = require('express');
 const router = express.Router();
 const Products = require('./products-model');
 
-router.get('/products', async(req, res) => {
+router.get('/', async(req, res) => {
     const products = await Products.getAllProducts()
     res.json(products);
 })
 
+router.get('/:id', async (req, res) => {
+    const product = await Products.getProductById(req.params.id);
+    res.json(product);
+  });
+
+router.post('/', async (req, res) => {
+    await Products.addProduct(req.body);
+    res.sendStatus(201);
+  });
+
+  router.put('/:id', async (req, res) => {
+    await Products.updateProduct(req.params.id, req.body);
+    res.sendStatus(200);
+  });
+
+router.delete('/:id', async (req, res) => {
+    await Products.deleteProduct(req.params.id);
+    res.sendStatus(200);
+  });
+
+router.get('/type/:type', async (req, res) => {
+    const products = await Products.getProductsByType(req.params.type);
+    res.json(products);
+  });
+
+router.get('/color/:color', async (req, res) => {
+    const products = await Products.getProductsByColor(req.params.color);
+    res.json(products);
+  });
+
+router.get('/brand/:brand', async (req, res) => {
+    const products = await Products.getProductsByBrand(req.params.brand);
+    res.json(products);
+  });
+
+router.get('/quantity-greater-than/:quantity', async (req, res) => {
+    const products = await Products.getProductsByQuantityGreaterThan(req.params.quantity);
+    res.json(products);
+  });
+
+router.get('/price-less-than/:price', async (req, res) => {
+    const products = await Products.getProductsByPriceLessThan(req.params.price);
+    res.json(products);
+  });
 
 module.exports = router
